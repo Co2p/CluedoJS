3df8b9157c161dbfb3f992b1883b7a4cae30fef1
Co2p <go.co2p@gmail.com>
2016-12-22 21:40:48 +0100

    switched the order

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ $('i').click((e) => {
   var currentIcon = e.currentTarget.innerHTML;
   if (currentIcon == 'check_box_outline_blank') {
     e.currentTarget.innerHTML = 'help';
-  } else if (currentIcon == 'help') {
-    e.currentTarget.innerHTML = 'check_box';
   } else if (currentIcon == 'check_box') {
     e.currentTarget.innerHTML = 'check_box_outline_blank';
+  } else if (currentIcon == 'help') {
+    e.currentTarget.innerHTML = 'check_box';
   }
 });
