4bf5801949546fdc3b89551f97cbc70fd72a84c2
Co2p <go.co2p@gmail.com>
2016-12-23 16:35:31 +0100

    Testing different icons

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 $('i').click((e) => {
   var currentIcon = e.currentTarget.innerHTML;
-  if (currentIcon == 'check_box_outline_blank') {
-    e.currentTarget.innerHTML = 'help';
-  } else if (currentIcon == 'help') {
-    e.currentTarget.innerHTML = 'check_box';
-  } else if (currentIcon == 'check_box') {
-    e.currentTarget.innerHTML = 'check_box_outline_blank';
+  if (currentIcon == 'remove_circle') {
+    e.currentTarget.innerHTML = 'radio_button_unchecked';
+  } else if (currentIcon == 'radio_button_unchecked') {
+    e.currentTarget.innerHTML = 'check_circle';
+  } else if (currentIcon == 'check_circle') {
+    e.currentTarget.innerHTML = 'add_circle';
+  } else if (currentIcon == 'add_circle') {
+    e.currentTarget.innerHTML = 'remove_circle';
   }
 });
 
