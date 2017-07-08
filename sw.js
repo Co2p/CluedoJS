8c1caf59bef4bbe5ebc4121757827402e34441fd
Co2p <go.co2p@gmail.com>
2017-07-08 18:38:15 +0200

    cleanup

--- sw.js
+++ sw.js
@@ -113,22 +113,16 @@ self.addEventListener("activate", (event) => {
   /* Just like with the install event, event.waitUntil blocks activate on a promise.
      Activation will fail unless the promise is fulfilled.
   */
-  //console.log('WORKER: activate event in progress.');
-
   event.waitUntil(
-    caches
-      /* This method returns a promise which will resolve to an array of available
-         cache keys.
-      */
-      .keys()
-      .then(function (keys) {
+    caches.keys()
+      .then((keys) => {
         // We return a promise that settles when all outdated caches are deleted.
         return Promise.all(
-          keys.filter(function (key) {
+          keys.filter((key) => {
               // Filter by keys that don't start with the latest version prefix.
               return !key.startsWith(version);
             })
-            .map(function (key) {
+            .map((key) => {
               /* Return a promise that's fulfilled
                  when each outdated cache is deleted.
               */
