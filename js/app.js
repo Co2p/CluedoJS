0970d76c6db7c4f974ed3095184797cb1cff1313
Gordon Cooper <go.co2p@gmail.com>
2021-09-30 22:18:44 +0200

    set cookies for checkbox state

--- js/app.js
+++ js/app.js
@@ -3,19 +3,23 @@ window.addEventListener('load', () => setTheme(Cookies.get('theme')), false);
 Array.from(document.getElementsByTagName("i")).forEach((e) => {
   e.addEventListener("click", (event) => {
     var currentIcon = event.currentTarget.innerHTML;
-  if (currentIcon == 'remove_circle') {
-    event.currentTarget.innerHTML = 'radio_button_unchecked';
-  } else if (currentIcon == 'radio_button_unchecked') {
-    event.currentTarget.innerHTML = 'check_circle';
-  } else if (currentIcon == 'check_circle') {
-    event.currentTarget.innerHTML = 'add_circle';
-  } else if (currentIcon == 'add_circle') {
-    event.currentTarget.innerHTML = 'remove_circle';
-  }
+    var newIcon = ''
+    if (currentIcon == 'remove_circle') {
+      newIcon = 'radio_button_unchecked';
+    } else if (currentIcon == 'radio_button_unchecked') {
+      newIcon = 'check_circle';
+    } else if (currentIcon == 'check_circle') {
+      newIcon = 'add_circle';
+    } else if (currentIcon == 'add_circle') {
+      newIcon = 'remove_circle';
+    }
+    event.currentTarget.innerHTML = newIcon;
+    Cookies.set(event.target.id, newIcon,  { expires: 60 });
+    console.log(Cookies.get(event.target.id));
   })
 })
 
-window.onbeforeunload = function() {
+window.onbeforeunload = function () {
   return "";
 }
 
@@ -31,11 +35,11 @@ document.getElementById("fab").addEventListener("click", (e) => {
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register(`https://cluedo.co2p.se/sw.js`)
-  .then(function(reg) {
-    // registration worked
-    console.log('Registration succeeded. Scope is ' + reg.scope);
-  }).catch(function(error) {
-    // registration failed
-    console.log('Registration failed with ' + error);
-  });
+    .then(function (reg) {
+      // registration worked
+      console.log('Registration succeeded. Scope is ' + reg.scope);
+    }).catch(function (error) {
+      // registration failed
+      console.log('Registration failed with ' + error);
+    });
 }
